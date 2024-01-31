82b729b68467d342db4eaf2dc58127cbb59916b1
Andrey Dmitriev <admit8490@gmail.com>
2024-02-01 01:03:12 +0200

    setting up empty Collection for commands

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+//  Require node modules
+const fs = require('node:fs');
+const path = require('node:path');
+
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 const dotenv = require('dotenv');
 dotenv.config();
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// commands
+
+client.commands = new Collection();
+
+console.log(client);
+console.log(client.commands)
 
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
