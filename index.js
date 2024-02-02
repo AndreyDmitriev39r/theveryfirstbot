573121732504bcb0b8952f720ca894ff66c576b3
Andrey Dmitriev <admit8490@gmail.com>
2024-02-03 00:36:09 +0200

    receiving interactions example

--- index.js
+++ index.js
@@ -40,5 +40,29 @@ client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
+// receiving interactions
+
+client.on(Events.InteractionCreate, async interaction => {
+	console.log("event triggered");
+	if (!interaction.isChatInputCommand()) return;
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
 // Log in to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN);
