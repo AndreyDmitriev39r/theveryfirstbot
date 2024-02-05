a615b5a1d01e3b58d11ff23fb96e81f9bff1d661
Andrey Dmitriev <admit8490@gmail.com>
2024-02-05 22:41:49 +0200

    syncing tags: database files

---
+++ db.js
@@ -0,0 +1,27 @@
+// ORM
+const Sequelize = require('sequelize');
+
+// database connection info
+const sequelize = new Sequelize('database', 'user', 'password', {
+	host: 'localhost',
+	dialect: 'sqlite',
+	logging: false,
+	storage: 'database.sqlite',
+})
+
+// creating a table
+const Tags = sequelize.define('tags', {
+	name: {
+		type: Sequelize.STRING,
+		unique: true,
+	},
+	description: Sequelize.TEXT,
+	username: Sequelize.STRING,
+	usage_count: {
+		type: Sequelize.INTEGER,
+		defaultValue: 0,
+		allowNull: false,
+	},
+});
+
+module.exports = { Tags };
