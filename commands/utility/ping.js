d44e3cf08d61d2dead9c6c04685b4ef64b6dce9c
Andrey Dmitriev <admit8490@gmail.com>
2024-02-06 20:13:22 +0200

    edit and defer replies on the ping example

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,10 +1,17 @@
 const { SlashCommandBuilder } = require('discord.js');
+const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		await interaction.reply({content: 'Pong!', ephemeral: true});
+		// waiting before initial reply for more than 3 seconds will cause trouble, before editReply - will not
+		// for time-consuming commands use
+		// deferReply() - it is alternate initial response
+		await wait(5500); 
+		// if initial reply is ephemeral, so does edit reply
+		await interaction.editReply('Again!');
 	},
 };
